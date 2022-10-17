81fba38f52d3f38c4b2febca4fb604a6caab30ac
1923k <soolekefuoe2@gmail.com>
2022-10-17 15:53:03 -0700

    Updated Task

--- functions1.c
+++ functions1.c
@@ -77,6 +77,23 @@ int flags, int width, int precision, int size)
 return (print_hexa(types, "0123456789abcdef", buffer,
 flags, 'x', width, precision, size));
 }
+/******** PRINT UNSIGNED NUMBER IN UPPER HEXADECIMAL ********/
+/**
+ * print_hexa_upper - Print an unsigned in upper hexadecimal notation
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags: Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_hexa_upper(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+return (print_hexa(types, "123456789AABCDEF", buffer
+flags, 'x', width, precision, size));
+}
 
 
 
