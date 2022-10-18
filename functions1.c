05e85bf8a403aa34d30af784a738648c8006cd83
1923k <soolekefuoe2@gmail.com>
2022-10-17 22:04:05 -0700

    Updated Task

--- functions1.c
+++ functions1.c
@@ -19,7 +19,7 @@ unsigned long int num = va_arg(types, unsigned long int);
 num = convert_size_unsignd(num, size);
 if (num == 0)
 buffer[i--] = '0';
-buffer[BUFF-SIZE - 1] = '\0';
+buffer[BUFF_SIZE - 1] = '\0';
 while (num > 0)
 {
 buffer[i--] = (num % 10) + '0';
@@ -36,10 +36,11 @@ return (write_unsgnd(0, i, buffer, flags, width, precision, size));
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Number of chars printed
+ * @flags: Calculates active flags
+ * Return: Number of chars printed.
  */
 int print_octal(va_list types, char buffer[],
-int flags, int width, int precision , int size)
+int flags, int width, int precision, int size)
 {
 int i = BUFF_SIZE - 2;
 unsigned long int num = va_arg(types, unsigned long int);
